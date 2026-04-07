adc000448fceb40086cc691737423049a77047b8
midetim <133310905+midetim@users.noreply.github.com>
2026-04-07 02:39:08 -0400

    Create TGChannelGetParameterTest.java

---
+++ common/TuxGuitar-lib/src/test/java/app/tuxguitar/song/models/ISPTests/TGChannelGetParameterTest.java
@@ -0,0 +1,44 @@
+package app.tuxguitar.song.models;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TGChannelGetParameterTest {
+
+    private static class TestTGChannel extends TGChannel { }
+
+    private static class TestTGChannelParameter extends TGChannelParameter { }
+
+    private TestTGChannelParameter makeParameter(String key, String value) {
+        TestTGChannelParameter param = new TestTGChannelParameter();
+        param.setKey(key);
+        param.setValue(value);
+        return param;
+    }
+
+    @Test
+    public void testNegativeIndexReturnsNull() {
+        TestTGChannel channel = new TestTGChannel();
+        channel.addParameter(makeParameter("k1", "v1"));
+
+        assertNull(channel.getParameter(-1));
+    }
+
+    @Test
+    public void testValidIndexReturnsParameter() {
+        TestTGChannel channel = new TestTGChannel();
+        channel.addParameter(makeParameter("k1", "v1"));
+        channel.addParameter(makeParameter("k2", "v2"));
+
+        assertEquals("k2", channel.getParameter(1).getKey());
+        assertEquals("v2", channel.getParameter(1).getValue());
+    }
+
+    @Test
+    public void testTooLargeIndexReturnsNull() {
+        TestTGChannel channel = new TestTGChannel();
+        channel.addParameter(makeParameter("k1", "v1"));
+
+        assertNull(channel.getParameter(5));
+    }
+}

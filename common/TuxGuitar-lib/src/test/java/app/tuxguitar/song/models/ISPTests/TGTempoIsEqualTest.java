02e86054d3d191902fb22de4548affda3cf14c9c
midetim <133310905+midetim@users.noreply.github.com>
2026-04-07 02:42:51 -0400

    Create TGTempoIsEqualTest for equality validation
    Add unit tests for TGTempo equality checks.

---
+++ common/TuxGuitar-lib/src/test/java/app/tuxguitar/song/models/ISPTests/TGTempoIsEqualTest.java
@@ -0,0 +1,53 @@
+package app.tuxguitar.song.models;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TGTempoIsEqualTest {
+
+    private static class TestTGTempo extends TGTempo { }
+
+    @Test
+    public void testAllFieldsEqualReturnsTrue() {
+        TestTGTempo t1 = new TestTGTempo();
+        TestTGTempo t2 = new TestTGTempo();
+
+        t1.setValueBase(120, TGDuration.QUARTER, false);
+        t2.setValueBase(120, TGDuration.QUARTER, false);
+
+        assertTrue(t1.isEqual(t2));
+    }
+
+    @Test
+    public void testDifferentRawValueReturnsFalse() {
+        TestTGTempo t1 = new TestTGTempo();
+        TestTGTempo t2 = new TestTGTempo();
+
+        t1.setValueBase(120, TGDuration.QUARTER, false);
+        t2.setValueBase(100, TGDuration.QUARTER, false);
+
+        assertFalse(t1.isEqual(t2));
+    }
+
+    @Test
+    public void testDifferentBaseReturnsFalse() {
+        TestTGTempo t1 = new TestTGTempo();
+        TestTGTempo t2 = new TestTGTempo();
+
+        t1.setValueBase(120, TGDuration.QUARTER, false);
+        t2.setValueBase(120, TGDuration.EIGHTH, false);
+
+        assertFalse(t1.isEqual(t2));
+    }
+
+    @Test
+    public void testDifferentDottedReturnsFalse() {
+        TestTGTempo t1 = new TestTGTempo();
+        TestTGTempo t2 = new TestTGTempo();
+
+        t1.setValueBase(120, TGDuration.QUARTER, false);
+        t2.setValueBase(120, TGDuration.QUARTER, true);
+
+        assertFalse(t1.isEqual(t2));
+    }
+}
